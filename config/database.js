ef8efe228bce49a9728705e80dbfc7a39d469bbe
gunooknam <ngotic@kw.ac.kr>
2018-05-20 21:14:57 +0900

    login

---
+++ config/database.js
@@ -0,0 +1,12 @@
+module.exports = function(){
+ var mysql = require('mysql');
+ var conn = mysql.createConnection({
+   host     : 'localhost',
+   port     :  3306,
+   user     : 'root',
+   password : '123456',
+   database : 'movie_hi'
+ });
+ conn.connect();
+ return conn;
+}
