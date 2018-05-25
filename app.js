56284e4052d9080a0726fa806f56fc92c6c05905
soonoo <qpseh2m7@gmail.com>
2018-05-25 19:57:45 +0900

    add movie register page

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var createError = require('http-errors');
 // var ejs = require("ejs").__express;
+
 var app = require('./config/express')();
 var passport = require('./config/passport')(app);
-// view engine setup
-var login = require('./routes/login')(passport);
+
+var loginRouter = require('./routes/login')(passport);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+var adminRouter = require('./routes/admin');
 
 app.use('/api', apiRouter);
-app.use('/auth', login);
+app.use('/auth', loginRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
