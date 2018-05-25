e313e00fa452273373811abd3abec0c7dc1d7237
JIYOUGN-PC\지영 <kch1120@kw.ac.kr>
2018-05-26 04:14:25 +0900

    moviedetail 페이지 수정

--- app.js
+++ app.js
@@ -8,14 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 var moviedetail = require('./routes/moviedetail');
-var temp = require('./routes/temp');
 
 app.use('/api', apiRouter);
 app.use('/auth', login);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/moviedetail', moviedetail);
-app.use('/temp', temp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
